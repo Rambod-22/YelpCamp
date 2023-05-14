0c650ce2039095ea7fc42bf22bce8fad17172a6b
Rambod Zafarmoghaddam <Rambod.z@gmail.com>
2023-05-14 14:30:32 -0400

    deploy

--- app.js
+++ app.js
@@ -22,11 +22,11 @@ const campgroundRoutes = require('./routes/campgrounds')
 const reviewRoutes = require('./routes/reviews')
 const User = require('./models/user')
 const mongoSanitize = require('express-mongo-sanitize');
-const dbUrl = 'mongodb://localhost:27017/yelp-camp';
+const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
 //'mongodb://localhost:27017/yelp-camp'
 const ExpressError = require('./helpers/ExpressError')
 const MongoDBStore = require('connect-mongo')(session);
-mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
