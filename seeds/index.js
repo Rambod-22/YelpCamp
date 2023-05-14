05b0f9eebf97ec47b0fa05e24ee046cdcbbd6e0b
Rambod Zafarmoghaddam <Rambod.z@gmail.com>
2023-05-14 14:53:13 -0400

    deploy

--- seeds/index.js
+++ seeds/index.js
@@ -2,7 +2,11 @@ const mongoose = require('mongoose');
 const cities = require('./cities')
 const { places, descriptors } = require('./seedHelpers')
 const Campground = require("../models/campground")
-mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp', {
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+  }
+  const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp'
+mongoose.connect(dbUrl, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
